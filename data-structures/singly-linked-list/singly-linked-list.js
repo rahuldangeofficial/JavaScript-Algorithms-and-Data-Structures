528d1cc6867631b3ae828fed2ecca247b9f6b60f
Rahul Dange <rahulkdange2002@gmail.com>
2023-11-04 19:00:30 +0530

    Update Singly Linked List

--- data-structures/singly-linked-list/singly-linked-list.js
+++ data-structures/singly-linked-list/singly-linked-list.js
@@ -9,182 +9,214 @@ class SinglyLinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
+    this.size = 0;
   }
 
   findIndex(element) {
     let index = 0;
     let pointer = this.head;
+
     while (pointer !== null) {
       if (pointer.data === element) {
         return index;
       }
+
       pointer = pointer.next;
       index += 1;
     }
+
     return -1;
   }
-
   findElement(index) {
     let currentIndex = 0;
     let pointer = this.head;
+
     while (pointer !== null) {
       if (currentIndex === index) {
         return pointer.data;
       }
-      currentIndex += 1;
+
       pointer = pointer.next;
+      currentIndex += 1;
     }
+
     return undefined;
   }
-
-  findPreviousNodeByElement(element) {
+  previousNodeByElement(element) {
     let pointer = this.head;
+
     while (pointer.next !== null) {
       if (pointer.next.data === element) {
         return pointer;
       }
+
       pointer = pointer.next;
     }
+
     return null;
   }
-
-  findPreviousNodeByIndex(index) {
-    if (this.head === null) {
-      return null;
-    }
-
-    if (index < 0 || index >= this.length()) {
-      throw new Error("Invalid index");
-    }
-
-    let currentIndex = 0;
+  previousNodeByIndex(index) {
     let pointer = this.head;
+    let currentIndex = 0;
+
     while (pointer.next !== null) {
-      if (currentIndex === index - 1) {
+      if (currentIndex + 1 === index) {
         return pointer;
       }
-      currentIndex += 1;
+
       pointer = pointer.next;
+      currentIndex += 1;
     }
 
     return null;
   }
-
   length() {
-    let count = 0;
+    return this.size;
+  }
+  getElements() {
     let pointer = this.head;
+    let elements = [];
+
     while (pointer !== null) {
-      count++;
+      elements.push(pointer.data);
       pointer = pointer.next;
     }
-    return count;
-  }
 
-  print() {
-    let pointer = this.head;
-    while (pointer !== null) {
-      console.log(pointer.data);
-      pointer = pointer.next;
-    }
+    return elements;
   }
-
   insertAtHead(data) {
-    let node = new Node(data);
-    node.next = this.head;
+    if (data === undefined) {
+      return false;
+    }
 
+    const node = new Node(data);
+
+    node.next = this.head;
     this.head = node;
+    this.size += 1;
+
     if (this.tail === null) {
       this.tail = this.head;
     }
+    return true;
   }
-
   insertAtTail(data) {
-    if (this.head === null) {
-      this.insertAtHead(data);
-      return;
+    if (data === undefined) {
+      return false;
     }
 
-    let node = new Node(data);
+    if (this.tail === null) {
+      return this.insertAtHead(data);
+    }
+
+    const node = new Node(data);
     this.tail.next = node;
-    this.tail = node;
-  }
-
-  insertAtIndex(index, data) {
-    if (index < 0 || index > this.length()) {
-      throw new Error("Invalid index");
-    }
-
-    if (index === 0) {
-      this.insertAtHead(data);
-      return true;
-    }
-
-    if (index === this.length()) {
-      this.insertAtTail(data);
-      return true;
-    }
-
-    let node = new Node(data);
-    let previousNode = this.findPreviousNodeByIndex(index);
-
-    node.next = previousNode.next;
-    previousNode.next = node;
+    this.size += 1;
 
     return true;
   }
+  insertAtIndex(index, data) {
+    if (index === undefined || data === undefined) {
+      return false;
+    }
+    if (index < 0 || index > this.size) {
+      return false;
+    }
+    if (index === 0) {
+      return this.insertAtHead(data);
+    }
+    if (index === this.size) {
+      return this.insertAtTail(data);
+    }
 
+    const node = new Node(data);
+
+    let prevNode = this.previousNodeByIndex(index);
+
+    node.next = prevNode.next;
+    prevNode.next = node;
+
+    this.size += 1;
+
+    return true;
+  }
   deleteAtHead() {
     if (this.head === null) {
-      throw new Error("Linked list is empty");
+      return false;
+    }
+
+    if (this.tail === this.head) {
+      this.head = null;
+      this.tail = null;
+      this.size -= 1;
+
+      return true;
     }
 
     this.head = this.head.next;
-    if (this.head === null) {
-      this.tail = null;
-    }
-  }
 
-  deleteAtTail() {
-    if (this.head === null) {
-      throw new Error("Linked list is empty");
-    }
-
-    if (this.head.next === null) {
-      this.head = null;
-      this.tail = null;
-      return;
-    }
-
-    let previousNode = this.findPreviousNodeByElement(this.tail.data);
-    this.tail = previousNode;
-    previousNode.next = null;
-  }
-
-  deleteAtIndex(index) {
-    if (index < 0 || index >= this.length()) {
-      throw new Error("Invalid index");
-    }
-
-    let previousNode = this.findPreviousNodeByIndex(index);
-
-    if (index === 0) {
-      this.deleteAtHead();
-      return true;
-    }
-
-    if (index === this.length() - 1) {
-      this.deleteAtTail();
-      return true;
-    }
-
-    previousNode.next = previousNode.next.next;
     return true;
   }
+  deleteAtTail() {
+    if (this.head === null) {
+      return false;
+    }
 
+    if (this.tail === this.head) {
+      return this.deleteAtHead();
+    }
+
+    let prevNode = this.previousNodeByIndex(this.size - 1);
+    prevNode.next = null;
+    this.tail = prevNode;
+    this.size -= 1;
+
+    return true;
+  }
+  deleteAtIndex(index) {
+    if (index < 0 || index >= this.length()) {
+      return false;
+    }
+    if (index === 0) {
+      return this.deleteAtHead();
+    }
+    if (index === this.length() - 1) {
+      return this.deleteAtTail();
+    }
+
+    let prevNode = this.previousNodeByIndex(index);
+    prevNode.next = prevNode.next.next;
+    this.size -= 1;
+
+    return true;
+  }
+  reverse() {
+    if (this.head === null) {
+      return false;
+    }
+
+    this.tail = this.head;
+
+    let prev = null;
+    let current = this.head;
+    let next = null;
+
+    while (current !== null) {
+      next = current.next;
+      current.next = prev;
+
+      prev = current;
+      current = next;
+    }
+
+    this.head = prev;
+
+    return true;
+  }
   clear() {
     this.head = null;
     this.tail = null;
   }
 }
 
-const list1 = new SinglyLinkedList();
+const sll1 = new SinglyLinkedList();
